359a075f1751b958634db74b4ff5d93804a80ec5
walkor <walkor@workerman.net>
2015-08-19 21:03:58 +0800

    save

--- src/Event/Emitter.php
+++ src/Event/Emitter.php
@@ -1,12 +1,86 @@
 <?php
 namespace Event;
-
 class Emitter
 {
     /**
-     * [event1=>[listener1, listener2, ..], event2=>[listener1, listener2, ..] ..]
-     * @var array
+     * [event=>[[listener1, once?], [listener2,once?], ..], ..]
      */
-    protected $allEvent = array();
+    protected $_eventListenerMap = array();
     
+    public function on($event_name, $listener)
+    {
+        $this->emit('newListener', array($event_name, $listener));
+        $this->_eventListenerMap[$event_name][] = array($listener, 0); 
+        return $this;
+    }
+
+    public function once($event_name, $listener)
+    {
+        $this->_eventListenerMap[$event_name][] = array($listener, 1);
+        return $this;
+    }
+   
+    public function removeListener($event_name, $listener)
+    {
+        if(!isset($this->_eventListenerMap[$event_name]))
+        {
+            return $this;
+        }
+        foreach($this->_eventListenerMap[$event_name] as $key=>$item)
+        {
+            if($item[0] === $listener)
+            {
+                $this->emit('removeListener', array($event_name, $listener));
+                unset($this->_eventListenerMap[$event_name][$key]);
+            }
+        }
+        if(empty($this->_eventListenerMap[$event_name]))
+        {
+            unset($this->_eventListenerMap[$event_name]);
+        }
+        return $this;
+    }
+
+    public function removeAllListeners($event_name)
+    {
+        $this->emit('removeListener', array($event_name));
+        unset($this->_eventListenerMap[$event_name]);
+        return $this;
+    }
+
+    public function listeners($event_name)
+    {
+        if(empty($this->_eventListenerMap[$event_name]))
+        {
+            return array();
+        }
+        $listeners = array();
+        foreach($this->_eventListenerMap[$event_name] as $item)
+        {
+            $listeners[] = $item[0];
+        }
+        return $listeners;
+    }
+
+    public function emit($event_name, $args = array())
+    {
+        if(empty($this->_eventListenerMap[$event_name]))
+        {
+            return false;
+        }
+        foreach($this->_eventListenerMap[$event_name] as $key=>$item)
+        {
+             call_user_func_array($item[0], $args);
+             // once ?
+             if($item[1])
+             {
+                 unset($this->_eventListenerMap[$event_name][$key]);
+                 if(!$this->_eventListenerMap[$event_name])
+                 {
+                     unset($this->_eventListenerMap[$event_name]);
+                 }
+             }
+        }
+        return true;
+    }
 }
