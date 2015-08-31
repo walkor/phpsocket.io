562b3f6c659794bc5aed75e3d27054001c84028b
walkor <worker-man@qq.com>
2015-08-31 22:54:38 +0800

    save

---
+++ src/Engine/Transport/WebSocket.php
@@ -0,0 +1,53 @@
+<?php
+namespace Engine\Transport;
+use \Engine\Transport;
+use \Engine\Parser;
+class WebSocket extends Transport
+{
+    public $writable = true;
+    public $supportsFraming = true;
+    public function __construct($req)
+    {
+        $self = $this;
+        $this->socket = $req->websocket;
+        $this->socket->onMessage = array($this. 'onData');
+        $this->socket->onClose = array($this, 'onClose');
+        $this->socket->onError = array($this, 'onError');
+        $this->socket->on('headers', function ($headers) {
+            //self.emit('headers', headers);
+        });
+        $this->writable = true;
+    }
+    
+    public function onData($connection, $data) 
+    {
+        call_user_func(array($this, 'parent::onData'), $data);
+    }
+    
+    public function onError($conection, $code, $msg)
+    {
+        call_user_func(array($this, 'parent::onClose'), $code, $msg);
+    }
+    
+    public function send($packets)
+    {
+        foreach($packets as $packet)
+        {
+            $self = $this;
+            Parser::encodePacket($packet, $this->supportsBinary, function($data)use($self) 
+            {
+                $self->socket->send(data);
+                $self->emit('drain');
+            });
+        }
+    }
+    
+    public function doClose($fn = null) 
+    {
+        $this->socket->close();
+        if(!empty($fn))
+        {
+            call_user_func($fn);
+        }
+    }
+}
