904727eb686acb4a1e89c7fb7f7cac82b69242e5
walkor <walkor@workerman.net>
2015-09-09 22:48:26 +0800

    save

--- examples/chat/server.php
+++ examples/chat/server.php
@@ -7,72 +7,70 @@ use Engine\Engine;
 include __DIR__ . '/../../src/autoload.php';    // for SocketIO class
 include __DIR__ . '/../../vendor/autoload.php'; // for workerman class
 
-//class_alias('\Engine\Protocols\Http', 'Protocols\SocketIO');
-
 $worker = new Worker('SocketIO://0.0.0.0:2020');
 $io = new SocketIO();
 $io->attach($worker);
 $io->on('connection', function($socket){
-        $socket->addedUser = false;
+    $socket->addedUser = false;
 
-        // when the client emits 'new message', this listens and executes
-        $socket->on('new message', function ($data)use($socket){
-            // we tell the client to execute 'new message'
-            $socket->broadcast->emit('new message', array(
-                'username'=> $socket->username,
-                'message'=> $data
+    // when the client emits 'new message', this listens and executes
+    $socket->on('new message', function ($data)use($socket){
+        // we tell the client to execute 'new message'
+        $socket->broadcast->emit('new message', array(
+            'username'=> $socket->username,
+            'message'=> $data
+        ));
+    });
+
+    // when the client emits 'add user', this listens and executes
+    $socket->on('add user', function ($username) use($socket){
+        global $usernames, $numUsers;
+        // we store the username in the socket session for this client
+        $socket->username = $username;
+        // add the client's username to the global list
+        $usernames[$username] = $username;
+        ++$numUsers;
+        $socket->addedUser = true;
+        $socket->emit('login', array( 
+            'numUsers' => $numUsers
+        ));
+        // echo globally (all clients) that a person has connected
+        $socket->broadcast->emit('user joined', array(
+            'username' => $socket->username,
+            'numUsers' => $numUsers
+        ));
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    $socket->on('typing', function () use($socket) {
+        $socket->broadcast->emit('typing', array(
+            'username' => $socket->username
+        ));
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    $socket->on('stop typing', function () use($socket) {
+        $socket->broadcast->emit('stop typing', array(
+            'username' => $socket->username
+        ));
+    });
+
+    // when the user disconnects.. perform this
+    $socket->on('disconnect', function () use($socket) {
+        global $usernames, $numUsers;
+        // remove the username from global usernames list
+        if($socket->addedUser) {
+            unset($usernames[$socket->username]);
+            --$numUsers;
+
+           // echo globally that this client has left
+           $socket->broadcast->emit('user left', array(
+               'username' => $socket->username,
+               'numUsers' => $numUsers
             ));
-        });
-
-        // when the client emits 'add user', this listens and executes
-        $socket->on('add user', function ($username) use($socket){
-            global $usernames, $numUsers;
-            // we store the username in the socket session for this client
-            $socket->username = $username;
-            // add the client's username to the global list
-            $usernames[$username] = $username;
-            ++$numUsers;
-            $socket->addedUser = true;
-            $socket->emit('login', array( 
-                'numUsers' => $numUsers
-            ));
-            // echo globally (all clients) that a person has connected
-            $socket->broadcast->emit('user joined', array(
-                'username' => $socket->username,
-                'numUsers' => $numUsers
-            ));
-        });
-
-        // when the client emits 'typing', we broadcast it to others
-        $socket->on('typing', function () use($socket) {
-            $socket->broadcast->emit('typing', array(
-                'username' => $socket->username
-            ));
-        });
-
-        // when the client emits 'stop typing', we broadcast it to others
-        $socket->on('stop typing', function () use($socket) {
-            $socket->broadcast->emit('stop typing', array(
-                'username' => $socket->username
-            ));
-        });
-
-        // when the user disconnects.. perform this
-        $socket->on('disconnect', function () use($socket) {
-            global $usernames, $numUsers;
-            // remove the username from global usernames list
-            if($socket->addedUser) {
-                unset($usernames[$socket->username]);
-                --$numUsers;
-
-               // echo globally that this client has left
-               $socket->broadcast->emit('user left', array(
-                   'username' => $socket->username,
-                   'numUsers' => $numUsers
-                ));
-            }
-       });
-    
+        }
+   });
+   
 });
 
 $web = new WebServer('http://0.0.0.0:2022');
