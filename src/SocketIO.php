c088f9964a02b22f907559f7168b2e52d593fdd8
Marcos Marcolin <marcos@ixcsoft.com.br>
2025-02-27 15:31:55 -0300

    feat: add support to ipv6.

--- src/SocketIO.php
+++ src/SocketIO.php
@@ -39,7 +39,8 @@ class SocketIO
             class_alias('PHPSocketIO\Engine\Protocols\SocketIO', 'Protocols\SocketIO');
         }
         if ($port) {
-            $worker = new Worker('SocketIO://[::]:' . $port, $opts);
+            $host = $opts['host'] ?? '0.0.0.0';
+            $worker = new Worker('SocketIO://' . $host . ':' . $port, $opts);
             $worker->name = 'PHPSocketIO';
 
             if (isset($opts['ssl'])) {
